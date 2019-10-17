27e927430a23feb5e20421a60dae5e220eba5a74
BryanCometa-HACK <100_Jornada1@FormacionSena.red>
2019-10-17 17:00:42 -0500

    Segunda modificación

--- FormNumeroVariable.aspx.cs
+++ FormNumeroVariable.aspx.cs
@@ -11,13 +11,20 @@ namespace RequerimientoWeb
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                txtNro1.Text = "0";
+                ViewState["guarda"] = 0;
+            }
             
         }
         int x = 0;
         protected void btnNro1_Click(object sender, EventArgs e)
         {
-            x++;
+            
+            x=(int)ViewState["guarda"]+1;
             txtNro1.Text = x.ToString();
+            ViewState["guarda"] = x;
         }
     }
 }
